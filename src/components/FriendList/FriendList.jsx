5d0ba609f906efe98844f3a09e7727b0b58e5e42
Denys iElkin <hvoarang@gmail.com>
2023-01-21 21:06:38 +0200

    fix

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,16 +1,45 @@
-import { FriendListItem } from './FriendListItem';
+// import { FriendListItem } from './FriendListItem';
+// import PropTypes from 'prop-types';
+// import s from './FriendList.module.css';
+
+// export const FriendList = ({ friends }) => {
+//   return (
+//     <ul className={s.friendList}>
+//       {friends.map(({ avatar, name, isOnline, id }) => (
+//         <FriendListItem
+//           key={id}
+//           isOnline={isOnline}
+//           avatar={avatar}
+//           name={name}
+//         />
+//       ))}
+//     </ul>
+//   );
+// };
+
+// FriendList.propTypes = {
+//   friends: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.number.isRequired,
+//       isOnline: PropTypes.bool.isRequired,
+//       avatar: PropTypes.string.isRequired,
+//       name: PropTypes.string.isRequired,
+//     })
+//   ),
+// };
 import PropTypes from 'prop-types';
-import s from './FriendList.module.css';
+import css from './FriendList.module.css';
+import { FriendListItem } from './FriendListItem';
 
 export const FriendList = ({ friends }) => {
   return (
-    <ul className={s.friendList}>
-      {friends.map(({ avatar, name, isOnline, id }) => (
+    <ul className={css.friend__list}>
+      {friends.map(friend => (
         <FriendListItem
-          key={id}
-          isOnline={isOnline}
-          avatar={avatar}
-          name={name}
+          key={friend.id}
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
         />
       ))}
     </ul>
@@ -19,11 +48,11 @@ export const FriendList = ({ friends }) => {
 
 FriendList.propTypes = {
   friends: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      isOnline: PropTypes.bool.isRequired,
+    PropTypes.exact({
       avatar: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
+      isOnline: PropTypes.bool.isRequired,
     })
   ),
 };
